904d1fb3a72d742be546e8c97dd136ab6ea30f38
afbaron22 <andres.baron@mercadolibre.com.co>
2022-01-18 11:30:57 -0500

    extension working and ready for deployment

--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
-
-
-function test(){
-    setInterval(function(){
-    var skip = document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text");
-    if(skip != undefined && skip.length > 0){
-        console.log("Add");
-        skip[0].click();
+function removeAdds() {
+  setInterval(function () {
+    const defined = v => v !== null && v !== undefined;
+    const ad = [...document.querySelectorAll(".ad-showing")][0];
+    if (defined(ad)) {
+      const video = document.querySelector("video");
+      if (defined(video)) {
+        video.currentTime = video.duration;
+      }
     }
-},3000)
+    var skip = document.getElementsByClassName(
+      "ytp-ad-text ytp-ad-skip-button-text"
+    );
+    if (skip != undefined && skip.length > 0) {
+      skip[0].click();
+    }
+  }, 500);
+  return function () {
+    clearTimeout(timeout);
+  };
 }
 
-test();
+removeAdds();
